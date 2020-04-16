e8cdac5faa91ff01df072ac0855b839b9e8b4292
Xuesong Wang <z5251072@ad.unsw.edu.au>
2020-04-17 01:12:47 +1000

    (shawn)change covType config

--- dataprocessing.py
+++ dataprocessing.py
@@ -101,6 +101,9 @@ def dataset_config(name):
     if name == 'sea':
         xpath = filepath + 'SEA_training_data.csv'
         ypath = filepath +'SEA_training_class.csv'
+    if name == 'covType':
+        xpath = filepath + 'covType_data.csv'
+        ypath = filepath + 'covType_label.csv'
     if name == 'Elec2':
         xpath = filepath + 'elec2_data.dat'
         ypath = filepath + 'elec2_label.dat'
